9a31fb72dfb2d28edd1e0c870d11cbb2b75a5bf5
Tiago Satiro <tiago.satiro@bdo.com.br>
2023-10-19 11:19:34 -0300

    Ajustes ApiRest

--- S7TechIntegracao.API/HttpRequestMessageExtensions.cs
+++ S7TechIntegracao.API/HttpRequestMessageExtensions.cs
@@ -17,19 +17,19 @@ namespace S7TechIntegracao.API
             var authority = req.RequestUri.Authority;               
             var scheme = req.RequestUri.Scheme;
             var port = req.RequestUri.Port;
-            //var baseUrl = req.GetRequestContext().VirtualPathRoot.TrimStart('/');
-            var baseUrl = req.RequestUri.AbsolutePath.Replace("/swagger/ui/index.html", "");
-            baseUrl = req.RequestUri.AbsolutePath.Replace("/swagger/ui/index", "");
+            var baseUrl = req.GetRequestContext().VirtualPathRoot.TrimStart('/');
+            //var baseUrl = req.RequestUri.AbsolutePath.Replace("/swagger/ui/index.html", "");
+            //baseUrl = req.RequestUri.AbsolutePath.Replace("/swagger/ui/index", "");
             var portaHttp = Convert.ToInt32(param["PortaHttp"]);
             var portaHttps = Convert.ToInt32(param["PortaHttps"]);
 
-#if DEBUG == false
+//#if DEBUG == false
 
-            if (port == 8082)
-                port = portaHttp;
-            else
-                port = portaHttps;
-#endif
+//            if (port == 8082)
+//                port = portaHttp;
+//            else
+//                port = portaHttps;
+//#endif
 
             //var uriBase = scheme + "://" + authority + ":" + port.ToString() + "/" + baseUrl;
             var uriBase = scheme + "://" + authority + baseUrl;
